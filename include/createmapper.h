dc53f0e746cc22203c33146c39fcf8a8bab6d075
Nero <rakshitdayal.allamraju@gmail.com>
2019-12-15 15:38:54 -0600

    added exception to publish cloud

--- include/createmapper.h
+++ include/createmapper.h
@@ -6,9 +6,15 @@
 #include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
+#include <std_msgs/Float32.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <std_msgs/Float32.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
 
 class Mapper
 {
@@ -24,6 +30,10 @@ public:
 
     boost::shared_ptr<tf::TransformListener> transform;
     sensor_msgs::PointCloud2 pc2_cloud_out;
+    sensor_msgs::PointCloud2 FullyCloud;
+    pcl::PointCloud<pcl::PointXYZ> Temp_full_cloud, Temp_scan_cloud;
+
+    bool GOTFIRST;
 
 };
 
--- src/createmapper.cpp
+++ src/createmapper.cpp
@@ -8,6 +8,7 @@ Mapper::Mapper(int argc, char **argv)
         //char** argv = NULL;
         ros::init(argc, argv, "MapperNode");
     }
+    this->GOTFIRST = false;
     this->mapnode.reset(new ros::NodeHandle("MapperNode"));
 
     this->lidarscan_sub = this->mapnode->subscribe("/pioneer2dxLidar/vlpscan", 1000, &Mapper::Lidar_Cb, this);
@@ -27,20 +28,43 @@ Mapper::~Mapper()
 
 void Mapper::Lidar_Cb(const sensor_msgs::PointCloud2 &msg)
 {
-    //std::cout << msg.header.frame_id << std::endl;
-
     sensor_msgs::PointCloud pcl_cloud_in, pcl_cloud_out;
-    sensor_msgs::convertPointCloud2ToPointCloud(msg, pcl_cloud_in);
     tf::StampedTransform tf_data;
-    try {
 
+    // convert the ros msg in PC2 to ros msg PC1
+    sensor_msgs::convertPointCloud2ToPointCloud(msg, pcl_cloud_in);
+
+    try {
+        // Look up the transform between odom frame and lidar frame
         this->transform->lookupTransform("odom", "lidar", ros::Time(0), tf_data);
+
+        // transform data obtained in lidar frame in ros PC1 to world frame (data again in ROS PC1)
         this->transform->transformPointCloud("odom", pcl_cloud_in, pcl_cloud_out);
 
+        // Convert the ros PC1 to ros PC2
         sensor_msgs::convertPointCloudToPointCloud2(pcl_cloud_out,this->pc2_cloud_out);
 
+        /*
+        // Convert the transformed ros PC1 data in odom frame into pcl pointcloud XYZRGB data
+        pcl::fromROSMsg(this->pc2_cloud_out, this->Temp_scan_cloud);
 
 
+        if(!this->GOTFIRST)
+        {
+            // First message is passed into fullcloud
+            this->Temp_full_cloud = this->Temp_scan_cloud;
+            this->GOTFIRST = true;
+        }
+        else {
+            // Append new message to full cloud
+            this->Temp_full_cloud += this->Temp_full_cloud + this->Temp_scan_cloud;
+
+        }
+
+        // convert back to ros PC2
+        pcl::toROSMsg(this->Temp_full_cloud, this->FullyCloud);
+*/
+        // Publish the message
         this->map_pub.publish(pc2_cloud_out);
 
 
@@ -50,34 +74,6 @@ void Mapper::Lidar_Cb(const sensor_msgs::PointCloud2 &msg)
 
         ros::Duration(1.0).sleep();
     }
-
-
-
-
-
-    /*
-    sensor_msgs::convertPointCloudToPointCloud2(pcl_cloud_out,this->pc2_cloud_out);
-
-    this->map_pub.publish(pc2_cloud_out);
-*/
-    /*
-    std::cout << "X = " << tf_data.getOrigin().x() << std::endl;
-    std::cout << "Y = " << tf_data.getOrigin().y() << std::endl;
-    std::cout << "Z = " << tf_data.getOrigin().z() << std::endl;
-
-    tf::Matrix3x3 m(tf_data.getRotation());
-    std::cout << "[" << m.getRow(0).getX() <<"," << m.getRow(0).getY() << "," <<m.getRow(0).getZ() << std::endl;
-    std::cout << m.getRow(1).getX() <<"," << m.getRow(1).getY() << "," <<m.getRow(1).getZ() << std::endl;
-    std::cout << m.getRow(2).getX() <<"," << m.getRow(2).getY() << "," <<m.getRow(2).getZ() << "]" << std::endl;
-
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-
-     std::cout << "Roll = " << roll*180/M_PI << std::endl;
-     std::cout << "Pitch = " << pitch*180/M_PI << std::endl;
-     std::cout << "Yaw = " << yaw*180/M_PI << std::endl;
-    std::cout <<"===============================" << std::endl;
-    */
 
 }
 
